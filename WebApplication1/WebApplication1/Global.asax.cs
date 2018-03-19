ec41965f25ed0c774144ced4780e1f30ffd96b2d
piyrw17 <anickhughes@gmail.com>
2018-03-19 16:08:36 -0400

    Lab7.3

---
+++ WebApplication1/WebApplication1/Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using WebApplication1.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using BootstrapIntroduction.DAL;
+
+namespace WebApplication1
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            var bookContext = new BookContext();
+            Database.SetInitializer(new BookInitializer());
+            bookContext.Database.Initialize(true);
+
+        }
+    }
+}
