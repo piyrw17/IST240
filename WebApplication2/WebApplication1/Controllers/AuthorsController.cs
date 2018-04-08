a8a5848c6bad1a07fc137df32708eaca789252a1
piyrw17 <anickhughes@gmail.com>
2018-04-07 22:41:26 -0400

    fgj

--- WebApplication2/WebApplication1/Controllers/AuthorsController.cs
+++ WebApplication2/WebApplication1/Controllers/AuthorsController.cs
@@ -53,7 +53,7 @@ namespace WebApplication1.Controllers
         // GET: Authors/Create
         public ActionResult Create()
         {
-            return View();
+            return View("Form", new Author());
         }
 
         // POST: Authors/Create
@@ -85,7 +85,7 @@ namespace WebApplication1.Controllers
             {
                 return HttpNotFound();
             }
-            return View(author);
+            return View("Form", author);
         }
 
         // POST: Authors/Edit/5
--- WebApplication2/WebApplication1/Models/Author.cs
+++ WebApplication2/WebApplication1/Models/Author.cs
@@ -3,21 +3,27 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using Newtonsoft.Json;
 
 namespace WebApplication1.Models
 {
     public class Author
     {
+        [JsonProperty(PropertyName = "id")]
         public int Id { get; set; }
         
         [Required]
+        [JsonProperty(PropertyName = "firstName")]
         public string FirstName { get; set; }
 
         [Required]
+        [JsonProperty(PropertyName = "lastName")]
         public string LastName { get; set; }
 
+        [JsonProperty(PropertyName = "biography")]
         public string Biography { get; set; }
 
+        [JsonProperty(PropertyName = "books")]
         public virtual ICollection<Book> Books { get; set; }
 
     }
